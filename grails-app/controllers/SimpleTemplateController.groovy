cceb5c86140e2ea9d2b9709457999051983441a2
Jeff Brown <jeff@jeffandbetsy.net>
2010-09-14 16:41:43 -0500

    don't append "Gemfire" as a suffix on the bean names

--- grails-app/controllers/SimpleTemplateController.groovy
+++ grails-app/controllers/SimpleTemplateController.groovy
@@ -1,28 +1,28 @@
 class SimpleTemplateController {
     
-    def region1Gemfire
+    def region1
     
     def removeFromCache = {
         def key = params.key
-        region1Gemfire.remove key
+        region1.remove key
         redirect action: 'displayCache'
     }
 
     def addToCache = {
         def key = params.key
         def value = params.value
-        region1Gemfire[key] = value
+        region1[key] = value
         redirect action: 'displayCache'
     }
     
     def retrieveFromCache = {
         def key = params.key
-        def value = region1Gemfire[key]
+        def value = region1[key]
         render "Retrieved value from cache... ${key} = ${value}"
     }
     
     def displayCache = {
-        def result = region1Gemfire.entrySet()
+        def result = region1.entrySet()
         [cacheEntries: result]
     }
 }
