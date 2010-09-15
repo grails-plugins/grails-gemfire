f21ba93e443e8df93710d35246c2adcb26301625
Jeff Brown <jeff@jeffandbetsy.net>
2010-09-15 13:54:13 -0500

    remove unused properties from plugin descriptor

--- GemfireGrailsPlugin.groovy
+++ GemfireGrailsPlugin.groovy
@@ -5,11 +5,11 @@ import org.grails.gemfire.RegionMetadataBuilder
 class GemfireGrailsPlugin {
     // the plugin version
     def version = "0.1-SNAPSHOT"
-    // the version or versions of Grails the plugin is designed for
+
+        // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.4 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
+
+        // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
             "grails-app/controllers/**",
@@ -17,7 +17,6 @@ class GemfireGrailsPlugin {
             "**/.gitignore"
     ]
 
-    // TODO Fill in these fields
     def author = "Jeff Brown"
     def authorEmail = "jeff.brown@springsource.com"
     def title = "Grails Gemfire Plugin"
@@ -28,10 +27,6 @@ data management platform.
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/gemfire"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
 
     def doWithSpring = {
         defaultGemfireCache(org.springframework.data.gemfire.CacheFactoryBean){}
@@ -62,24 +57,5 @@ data management platform.
                 }
             }
         }
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 }
