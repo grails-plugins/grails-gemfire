4a53266eb4f240737dc6f5a212e7ba189972d6b2
Jeff Brown <jeff@jeffandbetsy.net>
2010-09-15 15:55:22 -0500

    set version to 0.1-SNAPSHOT

--- GemfireGrailsPlugin.groovy
+++ GemfireGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.grails.gemfire.RegionMetadataBuilder
 
 class GemfireGrailsPlugin {
     // the plugin version
-    def version = "0.1.M1"
+    def version = "0.1-SNAPSHOT"
 
         // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.4 > *"
