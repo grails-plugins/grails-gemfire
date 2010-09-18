6b349ddd9c38b30ccd3bf2a35d8dfa671aa798c8
Jeff Brown <jeff@jeffandbetsy.net>
2010-09-18 10:17:51 -0700

    simplify the helper a bit by refactoring out some duplicate code.

--- src/groovy/org/grails/gemfire/GemfireHelper.groovy
+++ src/groovy/org/grails/gemfire/GemfireHelper.groovy
@@ -12,66 +12,54 @@ class GemfireHelper {
     }
     
     def query(q) {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.query q
-        } as GemfireCallback
-
-        gemfireTemplate.execute callback
+        }
     }
 
     def remove(key) {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.remove key
-        } as GemfireCallback
-
-        gemfireTemplate.execute callback
+        }
     }
     
     def put(key, value) {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.put key, value
-        } as GemfireCallback
-
-        gemfireTemplate.execute callback
+        } 
     }
     
     void putAt(key, value) {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.put key, value
-        } as GemfireCallback
-
-        gemfireTemplate.execute callback
+        }
     }
 
     def getAt(String key) {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.get key
-        } as GemfireCallback
-
-        gemfireTemplate.execute callback
+        }
     }
 
     def getAt(key) {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.get key
-        } as GemfireCallback
-
-        gemfireTemplate.execute callback
+        }
     }
 
     void putAt(String key, value) {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.put key, value
-        } as GemfireCallback
-
-        gemfireTemplate.execute callback
+        } 
     }
 
     def entrySet() {
-        def callback = { Region reg ->
+        invokeCallback { Region reg ->
             reg.entrySet()
-        } as GemfireCallback
-        
-        gemfireTemplate.execute(callback)        
+        }
+    }
+    
+    private invokeCallback(closure) {
+        gemfireTemplate.execute(closure as GemfireCallback)
     }
 }
