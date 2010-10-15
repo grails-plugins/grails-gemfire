a587992043af9ccd382db358feaa5c9158f5053b
Graeme Rocher <graeme.rocher@gmail.com>
2010-10-15 17:20:46 +0200

    Use gemfire XML generator instead of our own one

--- scripts/GemfireGenerate.groovy
+++ scripts/GemfireGenerate.groovy
@@ -2,16 +2,16 @@ includeTargets << grailsScript("_GrailsBootstrap")
 
 import org.grails.datastore.gorm.gemfire.config.*
 import org.springframework.datastore.mapping.core.*
+import com.gemstone.gemfire.internal.cache.xmlcache.*
 
 target(main: "The description of the script goes here!") {
 	bootstrap()
 	
 	def datastore = appCtx.getBean(Datastore)
-	
-	def generator = new CacheServerConfigGenerator(datastore)
-	
 	println "Generating cache.xml to ${grailsSettings.projectTargetDir}/cache.xml"
-	generator.generate(new File("${grailsSettings.projectTargetDir}/cache.xml"))
+	new File("${grailsSettings.projectTargetDir}/cache.xml").withPrintWriter {
+		CacheXmlGenerator.generate((com.gemstone.gemfire.cache.Cache)datastore.gemfireCache, it);		
+	}
 	println "Done."
 }
 
