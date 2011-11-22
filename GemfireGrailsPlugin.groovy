d8258ddd8b9e0a41a7763f732aa520ce32cde3f8
Jeff Brown <jeff@jeffandbetsy.net>
2011-11-22 09:18:23 -0600

    Express a dependency on Grails 2.0 > *

--- GemfireGrailsPlugin.groovy
+++ GemfireGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class GemfireGrailsPlugin {
     def version = "1.0.0.BUILD-SNAPSHOT"
 
         // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.4 > *"
+    def grailsVersion = "2.0 > *"
 
         // resources that are excluded from plugin packaging
     def pluginExcludes = [
